c73516d1d2fe9580b2197264a0c9cfbc690cb5dd
Homitowen <pycgames@gmail.com>
2022-12-06 17:53:05 -0300

    add primera forma de usuario. completar

--- src/main/java/luceroraul/stacktrace/examen/entities/Usuario.java
+++ src/main/java/luceroraul/stacktrace/examen/entities/Usuario.java
@@ -1,4 +1,28 @@
 package luceroraul.stacktrace.examen.entities;
 
-public class Usuario {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Usuario extends Identificable {
+    @Column(name = "dni")
+    private Integer DNI;
+
+
+    private List<String> nombres;
+    private List<String> apellidos;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "telefono")
+    private String telefono;
+
+    @ManyToOne(cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "sexo_id", nullable = false)
+    private Sexo sexo;
+
 }

9627a9ca573a8ec2445ac061f26e6ffe1817d7cf
Lucero Raul <luceroraul12@gmail.com>
2022-12-07 12:29:47 -0300

    add BilleteraRepository

---
+++ src/main/java/luceroraul/stacktrace/examen/repositories/BilleteraRepository.java
@@ -0,0 +1,7 @@
+package luceroraul.stacktrace.examen.repositories;
+
+import luceroraul.stacktrace.examen.entities.Billetera;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BilleteraRepository extends JpaRepository<Billetera, Long> {
+}

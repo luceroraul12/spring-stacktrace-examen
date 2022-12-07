8c73e5ae83cdbed28e168a509ccdb7e2fbfa389c
Lucero Raul <luceroraul12@gmail.com>
2022-12-07 13:54:01 -0300

    add ruta

--- src/main/java/luceroraul/stacktrace/examen/controllers/MonedaCriptoController.java
+++ src/main/java/luceroraul/stacktrace/examen/controllers/MonedaCriptoController.java
@@ -1,8 +1,10 @@
 package luceroraul.stacktrace.examen.controllers;
 
 import luceroraul.stacktrace.examen.entities.MonedaCripto;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("divisas")
 public class MonedaCriptoController extends ControllerBase<MonedaCripto>{
 }

d0697850e09d497cdfc461d6339176af8bacb6be
Lucero Raul <luceroraul12@gmail.com>
2022-12-07 11:40:12 -0300

    completando usuario entity

--- src/main/java/luceroraul/stacktrace/examen/entities/Usuario.java
+++ src/main/java/luceroraul/stacktrace/examen/entities/Usuario.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -12,17 +13,21 @@ import java.util.List;
 public class Usuario extends Identificable {
     @Column(name = "dni")
     private Integer DNI;
-
-
-    private List<String> nombres;
-    private List<String> apellidos;
-    @Column(name = "email")
-    private String email;
-    @Column(name = "telefono")
-    private String telefono;
-
     @ManyToOne(cascade = CascadeType.ALL, optional = false)
     @JoinColumn(name = "sexo_id", nullable = false)
     private Sexo sexo;
 
+    @ElementCollection
+    @Column(name = "nombre", length = 50)
+    @CollectionTable(name = "usuario_nombres", joinColumns = @JoinColumn(name = "owner_id"))
+    private List<String> nombres = new ArrayList<>();
+    @ElementCollection
+    @Column(name = "apellido", length = 50)
+    @CollectionTable(name = "Usuario_apellidos", joinColumns = @JoinColumn(name = "owner_id"))
+    private List<String> apellidos = new ArrayList<>();
+
+    @Column(name = "email")
+    private String email;
+    @Column(name = "telefono")
+    private String telefono;
 }

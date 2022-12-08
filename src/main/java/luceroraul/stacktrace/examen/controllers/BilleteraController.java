aff3bb4c92c6cbfcea6f10028af4d7c4a46dee6c
Lucero Raul <luceroraul12@gmail.com>
2022-12-08 17:38:05 -0300

    fix nombre tabla

--- src/main/java/luceroraul/stacktrace/examen/controllers/BilleteraController.java
+++ src/main/java/luceroraul/stacktrace/examen/controllers/BilleteraController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("billetera")
+@RequestMapping("billeteras")
 public class BilleteraController extends ControllerBase<Billetera>{
 }

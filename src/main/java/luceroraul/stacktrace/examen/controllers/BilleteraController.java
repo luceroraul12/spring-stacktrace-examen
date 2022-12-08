6e1155b3a8716d96fb15d861720a7e51bc9c1e6c
Lucero Raul <luceroraul12@gmail.com>
2022-12-08 18:10:32 -0300

    add TODO

--- src/main/java/luceroraul/stacktrace/examen/controllers/BilleteraController.java
+++ src/main/java/luceroraul/stacktrace/examen/controllers/BilleteraController.java
@@ -7,4 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("billeteras")
 public class BilleteraController extends ControllerBase<Billetera>{
+    //TODO: al modificar billeteras realiza la accion correctamente pero arroja un error relacionado a JSON
 }

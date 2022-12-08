94106215a843d325f50b4f494af5b44c9eeadd27
Lucero Raul <luceroraul12@gmail.com>
2022-12-08 17:38:34 -0300

    add allargs

--- src/main/java/luceroraul/stacktrace/examen/entities/MonedaCriptoCantidadAdquirida.java
+++ src/main/java/luceroraul/stacktrace/examen/entities/MonedaCriptoCantidadAdquirida.java
@@ -1,21 +1,23 @@
 package luceroraul.stacktrace.examen.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class MonedaCriptoCantidadAdquirida extends Identificable{
     @OneToOne
     @JoinColumn(name = "moneda_cripto_id")
     private MonedaCripto monedaCripto;
     @Column(name = "cantidad_adquirida")
-    private Double cantidadAdquirida = 0.0;
+    private Double cantidadAdquirida ;
 
     @ManyToOne
     @JoinColumn(name = "billetera_id")
     private Billetera billetera;
+
 
 }

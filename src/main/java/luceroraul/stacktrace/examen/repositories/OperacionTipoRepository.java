e51da4891f5f1ac202c67ec60c9324d768248ea3
Lucero Raul <luceroraul12@gmail.com>
2022-12-08 09:16:13 -0300

    fix entidad JPA

--- src/main/java/luceroraul/stacktrace/examen/repositories/OperacionTipoRepository.java
+++ src/main/java/luceroraul/stacktrace/examen/repositories/OperacionTipoRepository.java
@@ -1,6 +1,7 @@
 package luceroraul.stacktrace.examen.repositories;
 
+import luceroraul.stacktrace.examen.entities.OperacionTipo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OperacionTipoRepository extends JpaRepository<OperacionTipoRepository, Long> {
+public interface OperacionTipoRepository extends JpaRepository<OperacionTipo, Long> {
 }

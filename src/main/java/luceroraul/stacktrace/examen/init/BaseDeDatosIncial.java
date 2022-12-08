89b2737b7c64f90d8ef49ef096194e414c26a5cc
Lucero Raul <luceroraul12@gmail.com>
2022-12-08 10:27:33 -0300

    add metodo inicial para cargar tablas segun corresponda

---
+++ src/main/java/luceroraul/stacktrace/examen/init/BaseDeDatosIncial.java
@@ -0,0 +1,66 @@
+package luceroraul.stacktrace.examen.init;
+
+import jakarta.annotation.PostConstruct;
+import luceroraul.stacktrace.examen.entities.*;
+import luceroraul.stacktrace.examen.repositories.MonedaCriptoRepository;
+import luceroraul.stacktrace.examen.repositories.MonedaTradicionalRepository;
+import luceroraul.stacktrace.examen.repositories.OperacionTipoRepository;
+import luceroraul.stacktrace.examen.repositories.SexoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BaseDeDatosIncial {
+
+    @Autowired
+    SexoRepository sexoRepository;
+
+    @Autowired
+    MonedaTradicionalRepository monedaTradicionalRepository;
+
+    @Autowired
+    MonedaCriptoRepository monedaCriptoRepository;
+
+    @Autowired
+    OperacionTipoRepository operacionTipoRepository;
+
+    @PostConstruct
+    public void cargarBaseDeDatos(){
+        if(sexoRepository.count() == 0){
+            cargarTablaSexo();
+        }
+        if (monedaTradicionalRepository.count() == 0){
+            cargarTablaMonedaTradicional();
+        }
+        if (monedaCriptoRepository.count() == 0){
+            cargarTablaMonedaCripto();
+        }
+        if(operacionTipoRepository.count() == 0){
+            cargarTableOperacionTipo();
+        }
+    }
+
+    private void cargarTableOperacionTipo() {
+        operacionTipoRepository.save(new OperacionTipo("INTERCAMBIO"));
+        operacionTipoRepository.save(new OperacionTipo("DEPOSITO"));
+    }
+
+    private void cargarTablaMonedaCripto() {
+        monedaCriptoRepository.save(new MonedaCripto("BTC", 100.0));
+        monedaCriptoRepository.save(new MonedaCripto("ETH", 20.0));
+    }
+
+    private void cargarTablaMonedaTradicional() {
+        monedaTradicionalRepository.save(new MonedaTradicional("USD", 1.0));
+        monedaTradicionalRepository.save(new MonedaTradicional("ARGS", 169.81));
+    }
+
+
+    private void cargarTablaSexo() {
+
+        sexoRepository.save(new Sexo("masculino"));
+        sexoRepository.save(new Sexo("femenino"));
+        sexoRepository.save(new Sexo("otro"));
+        sexoRepository.save(new Sexo("no incluir"));
+    }
+}

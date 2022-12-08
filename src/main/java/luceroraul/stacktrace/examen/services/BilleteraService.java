05ceef88e4fd5d3d4f9e076576a14f987ba09854
Lucero Raul <luceroraul12@gmail.com>
2022-12-08 18:08:54 -0300

    ver

--- src/main/java/luceroraul/stacktrace/examen/services/BilleteraService.java
+++ src/main/java/luceroraul/stacktrace/examen/services/BilleteraService.java
@@ -1,8 +1,40 @@
 package luceroraul.stacktrace.examen.services;
 
 import luceroraul.stacktrace.examen.entities.Billetera;
+import luceroraul.stacktrace.examen.entities.MonedaCripto;
+import luceroraul.stacktrace.examen.entities.MonedaCriptoCantidadAdquirida;
+import luceroraul.stacktrace.examen.repositories.MonedaCriptoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BilleteraService extends ServiceABM<Billetera>{
+    @Autowired
+    MonedaCriptoRepository monedaCriptoRepository;
+
+    private Billetera billeteraSeleccionada;
+
+//    @Override
+//    public ResponseEntity<Object> crear(Billetera elemento) {
+//        Billetera respuesta;
+//        billeteraSeleccionada = elemento;
+//
+//        List<MonedaCripto> monedasCriptoDisponibles = monedaCriptoRepository.findAll();
+//        List<MonedaCriptoCantidadAdquirida> valoresIniciales = monedasCriptoDisponibles.stream()
+//                .map(this::adaptarMonedaCripto)
+//                .toList();
+//
+//        elemento.setMonedaCriptoCantidadAdquiridas(valoresIniciales);
+//
+//        respuesta = repository.save(elemento);
+//
+//        return super.crear(respuesta);
+//    }
+
+    private MonedaCriptoCantidadAdquirida adaptarMonedaCripto(MonedaCripto moneda){
+        return new MonedaCriptoCantidadAdquirida(moneda, 0.0, billeteraSeleccionada);
+    }
 }

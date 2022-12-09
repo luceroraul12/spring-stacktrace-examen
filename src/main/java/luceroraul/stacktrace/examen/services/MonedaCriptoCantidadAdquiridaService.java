3ee78146625a6e4159f7cea71f4a6568a3893e36
Lucero Raul <luceroraul12@gmail.com>
2022-12-09 19:20:15 -0300

    falto @Service

--- src/main/java/luceroraul/stacktrace/examen/services/MonedaCriptoCantidadAdquiridaService.java
+++ src/main/java/luceroraul/stacktrace/examen/services/MonedaCriptoCantidadAdquiridaService.java
@@ -1,7 +1,9 @@
 package luceroraul.stacktrace.examen.services;
 
 import luceroraul.stacktrace.examen.entities.MonedaCriptoCantidadAdquirida;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MonedaCriptoCantidadAdquiridaService extends ServiceABM<MonedaCriptoCantidadAdquirida>{
     @Override
     protected Class<MonedaCriptoCantidadAdquirida> recuperarClaseGenerica() {

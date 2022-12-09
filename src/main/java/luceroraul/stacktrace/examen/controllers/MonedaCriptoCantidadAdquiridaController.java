9dd65c5a10b4691065de9d628fe4de113315f402
Lucero Raul <luceroraul12@gmail.com>
2022-12-09 19:08:24 -0300

    add controller de activos

---
+++ src/main/java/luceroraul/stacktrace/examen/controllers/MonedaCriptoCantidadAdquiridaController.java
@@ -0,0 +1,10 @@
+package luceroraul.stacktrace.examen.controllers;
+
+import luceroraul.stacktrace.examen.entities.MonedaCriptoCantidadAdquirida;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("billeteras/activos")
+public class MonedaCriptoCantidadAdquiridaController extends ControllerBase<MonedaCriptoCantidadAdquirida> {
+}

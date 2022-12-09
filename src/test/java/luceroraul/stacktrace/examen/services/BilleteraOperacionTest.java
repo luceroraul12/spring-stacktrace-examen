6ace7f1c0a21f59580ea15e6ab202d40f524c97a
Lucero Raul <luceroraul12@gmail.com>
2022-12-09 19:07:38 -0300

    add test operaciones

---
+++ src/test/java/luceroraul/stacktrace/examen/services/BilleteraOperacionTest.java
@@ -0,0 +1,23 @@
+package luceroraul.stacktrace.examen.services;
+
+import luceroraul.stacktrace.examen.request.MonedaCriptoOperacionPeticion;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BilleteraOperacionTest {
+
+    BilleteraOperacionService service;
+
+    @Test
+    void depositar() {
+        MonedaCriptoOperacionPeticion peticion = new MonedaCriptoOperacionPeticion(
+
+        )
+        service.depositar()
+    }
+
+    @Test
+    void realizarDeposito() {
+    }
+}

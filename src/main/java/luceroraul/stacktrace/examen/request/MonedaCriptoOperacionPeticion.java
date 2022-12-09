820980710bb59a71eeaa5e69acc017c9f0223cdb
Lucero Raul <luceroraul12@gmail.com>
2022-12-09 18:31:33 -0300

    add peticion para operaciones con monedacripto

---
+++ src/main/java/luceroraul/stacktrace/examen/request/MonedaCriptoOperacionPeticion.java
@@ -0,0 +1,10 @@
+package luceroraul.stacktrace.examen.request;
+
+import luceroraul.stacktrace.examen.entities.OperacionTipo;
+
+public class MonedaCriptoOperacionPeticion {
+    private Long idMonedaCriptoCantidadAdquiridaOrigen;
+    private Long idMonedaCriptoCantidadAdquiridaDestino;
+    private OperacionTipo operacionTipo;
+    private Double cantidadOperable;
+}

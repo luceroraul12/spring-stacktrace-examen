332e2545b67f404ec8a2caf55dd632f25ed40e76
Lucero Raul <luceroraul12@gmail.com>
2022-12-09 18:36:51 -0300

    add argumentos

--- src/main/java/luceroraul/stacktrace/examen/controllers/divisas/OperacionesDivisasEnBilletera.java
+++ src/main/java/luceroraul/stacktrace/examen/controllers/divisas/OperacionesDivisasEnBilletera.java
@@ -1,10 +1,11 @@
 package luceroraul.stacktrace.examen.controllers.divisas;
 
 import luceroraul.stacktrace.examen.entities.MonedaCriptoCantidadAdquirida;
+import luceroraul.stacktrace.examen.request.MonedaCriptoOperacionPeticion;
 import org.springframework.http.ResponseEntity;
 
 public interface OperacionesDivisasEnBilletera {
 
-    ResponseEntity<Object> depositarCantidad();
-    ResponseEntity<Object> intercambiarCantidad();
+    ResponseEntity<Object> depositarCantidad(MonedaCriptoOperacionPeticion peticion);
+    ResponseEntity<Object> intercambiarCantidad(MonedaCriptoOperacionPeticion Peticion);
 }

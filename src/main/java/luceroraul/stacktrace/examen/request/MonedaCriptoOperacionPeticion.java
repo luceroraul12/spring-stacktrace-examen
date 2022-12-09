362c260b0e2bd119ab25f9b7a78e8a11888791a9
Lucero Raul <luceroraul12@gmail.com>
2022-12-09 19:06:39 -0300

    add lombok

--- src/main/java/luceroraul/stacktrace/examen/request/MonedaCriptoOperacionPeticion.java
+++ src/main/java/luceroraul/stacktrace/examen/request/MonedaCriptoOperacionPeticion.java
@@ -1,7 +1,13 @@
 package luceroraul.stacktrace.examen.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import luceroraul.stacktrace.examen.entities.OperacionTipo;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class MonedaCriptoOperacionPeticion {
     private Long idMonedaCriptoCantidadAdquiridaOrigen;
     private Long idMonedaCriptoCantidadAdquiridaDestino;

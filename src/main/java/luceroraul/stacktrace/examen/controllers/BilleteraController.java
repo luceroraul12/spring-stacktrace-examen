d85c2f5a335fec434802dda446068115053e7d3d
Lucero Raul <luceroraul12@gmail.com>
2022-12-09 17:59:17 -0300

    add get para consultar billeteras

--- src/main/java/luceroraul/stacktrace/examen/controllers/BilleteraController.java
+++ src/main/java/luceroraul/stacktrace/examen/controllers/BilleteraController.java
@@ -1,11 +1,23 @@
 package luceroraul.stacktrace.examen.controllers;
 
 import luceroraul.stacktrace.examen.entities.Billetera;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import luceroraul.stacktrace.examen.services.BilleteraService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("billeteras")
 public class BilleteraController extends ControllerBase<Billetera>{
     //TODO: al modificar billeteras realiza la accion correctamente pero arroja un error relacionado a JSON
+
+    @Autowired
+    BilleteraService billeteraService;
+
+    @GetMapping("consulta")
+    public ResponseEntity<Billetera> consultar(@RequestBody Map<String,Object> pedido){
+        return billeteraService.consultar(Long.valueOf(String.valueOf(pedido.get("id"))));
+    }
 }

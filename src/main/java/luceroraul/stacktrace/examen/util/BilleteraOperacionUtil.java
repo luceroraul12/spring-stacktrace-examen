d43236b4253d351516433c3d8f3cb1dfaa0bd0c1
Lucero Raul <luceroraul12@gmail.com>
2022-12-10 19:27:26 -0300

    add meotod conversion diferentes monedas y refactor metodos de incremento

--- src/main/java/luceroraul/stacktrace/examen/util/BilleteraOperacionUtil.java
+++ src/main/java/luceroraul/stacktrace/examen/util/BilleteraOperacionUtil.java
@@ -5,9 +5,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class BilleteraOperacionUtil {
-    public Activo realizarIncremento(Activo activo, Double cantidad) {
+    public Activo realizarIncrementoMismaUnidad(Activo activo, Double cantidad) {
         activo.setCantidadAdquirida(activo.getCantidadAdquirida() + cantidad);
         return activo;
+    }
+
+    public Activo realizarIncrementoDiferentesUnidades(Activo activoOrigen, Activo activoDestino, Double cantidadParaIncrementarOriginal){
+        Double precioUnitarioMonedaOrigenEnPesos = activoOrigen.getMonedaCripto().getRelacionDolar();
+        Double precioUnitarioMonedaDestinoEnPesos = activoDestino.getMonedaCripto().getRelacionDolar();
+        Double cantidadParaIncrementarEnPesos = precioUnitarioMonedaOrigenEnPesos * cantidadParaIncrementarOriginal;
+        Double cantidadParaIncrementarEnMonedaDestino = cantidadParaIncrementarEnPesos / precioUnitarioMonedaDestinoEnPesos;
+
+        activoDestino.setCantidadAdquirida(activoDestino.getCantidadAdquirida() + cantidadParaIncrementarEnMonedaDestino);
+
+        return activoDestino;
     }
 
     public Activo realizarReduccion(Activo activo, Double cantidad) {

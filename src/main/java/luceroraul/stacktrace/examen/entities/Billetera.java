49960fafdac5b6136fe790946f8619a78370fb5a
Lucero Raul <luceroraul12@gmail.com>
2022-12-10 10:04:43 -0300

    add args

--- src/main/java/luceroraul/stacktrace/examen/entities/Billetera.java
+++ src/main/java/luceroraul/stacktrace/examen/entities/Billetera.java
@@ -1,16 +1,17 @@
 package luceroraul.stacktrace.examen.entities;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
+@ToString
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Billetera extends Identificable{
     @ManyToOne(optional = false)
     @JoinColumn(name = "usuario_id", nullable = false)
@@ -19,4 +20,5 @@ public class Billetera extends Identificable{
 
     @OneToMany(mappedBy = "billetera", orphanRemoval = true)
     private List<Activo> activos = new ArrayList<>();
+
 }

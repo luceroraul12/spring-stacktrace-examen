23c711f766743563b5e96e88f8eb02e52b6f8159
Lucero Raul <luceroraul12@gmail.com>
2022-12-10 19:26:47 -0300

    add test conversion diferentes monedas

--- src/test/java/luceroraul/stacktrace/examen/util/BilleteraOperacionUtilTest.java
+++ src/test/java/luceroraul/stacktrace/examen/util/BilleteraOperacionUtilTest.java
@@ -1,8 +1,8 @@
 package luceroraul.stacktrace.examen.util;
 
 import luceroraul.stacktrace.examen.entities.Activo;
+import luceroraul.stacktrace.examen.entities.MonedaCripto;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,7 +18,7 @@ class BilleteraOperacionUtilTest {
 //                null,
 //                10.0);
         Activo original = new Activo(null, 20.5, null);
-        Activo resultado = util.realizarIncremento(original, 25.3);
+        Activo resultado = util.realizarIncrementoMismaUnidad(original, 25.3);
 
         assertEquals(45.8, resultado.getCantidadAdquirida());
         assertNotEquals(30, resultado.getCantidadAdquirida());
@@ -36,5 +36,20 @@ class BilleteraOperacionUtilTest {
         assertTrue(respuestaBuena);
         assertTrue(respuestaLimite);
         assertFalse(respuestaMala);
+    }
+
+    @Test
+    void conversionDiferentesMonedaseIncrementarDestino(){
+        MonedaCripto monedaOrigen = new MonedaCripto("mOrigen", 20.0);
+        MonedaCripto monedaDestino = new MonedaCripto("mDestino", 30.0);
+        Activo activoOrigen = new Activo(monedaOrigen, 10.0, null);
+        Activo activoDestino = new Activo(monedaDestino, 10.0, null);
+
+        Activo resultado = util.realizarIncrementoDiferentesUnidades(
+                activoOrigen,
+                activoDestino,
+                1.0);
+        assertEquals(10.66, resultado.getCantidadAdquirida(), 0.01);
+
     }
 }

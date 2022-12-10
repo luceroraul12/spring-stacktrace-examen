f0bb6ec715cef1981b118a17f1bdd3115c47ac40
Lucero Raul <luceroraul12@gmail.com>
2022-12-10 11:31:00 -0300

    add test

---
+++ src/test/java/luceroraul/stacktrace/examen/services/BilleteraOperacionServiceTest.java
@@ -0,0 +1,58 @@
+package luceroraul.stacktrace.examen.services;
+
+import luceroraul.stacktrace.examen.entities.Activo;
+import luceroraul.stacktrace.examen.repositories.ActivoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BilleteraOperacionServiceTest {
+
+    @MockBean
+    ActivoRepository repository;
+
+    @Autowired
+    BilleteraOperacionService service;
+
+    @BeforeEach
+    void before(){
+        MockitoAnnotations.openMocks(this);
+        Mockito.when(repository.findById(20L)).thenReturn(Optional.of(new Activo(null, 25.3, null)));
+        Mockito.when(repository.findById(10L)).thenReturn(Optional.of(new Activo(null, 15.0, null)));
+    }
+
+    @Test
+    void realizarIncremento() throws Exception {
+//        PeticionDeposito peticion = new PeticionDeposito(
+//                20L,
+//                10L,
+//                null,
+//                10.0);
+        Activo original = new Activo(null, 20.5, null);
+        Activo resultado = service.realizarIncremento(original, 25.3);
+
+        assertEquals(45.8, resultado.getCantidadAdquirida());
+        assertNotEquals(30, resultado.getCantidadAdquirida());
+        assertNotEquals(3000, resultado.getCantidadAdquirida());
+
+    }
+
+    @Test
+    void tieneMontoSuficiente() {
+        boolean respuestaBuena = service.tieneMontoSuficiente(20L,15.2);
+        boolean respuestaMala = service.tieneMontoSuficiente(20L,300.2);
+        boolean respuestaLimite = service.tieneMontoSuficiente(20L,25.3);
+        assertTrue(respuestaBuena);
+        assertTrue(respuestaLimite);
+        assertFalse(respuestaMala);
+    }
+}

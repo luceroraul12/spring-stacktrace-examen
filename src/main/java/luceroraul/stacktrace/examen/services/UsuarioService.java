35175df2695e93533fce3e82cb94a4361e8cfd27
Lucero Raul <luceroraul12@gmail.com>
2022-12-10 10:05:33 -0300

    modifico crear para que cree una billetera luego de crear un usuario

--- src/main/java/luceroraul/stacktrace/examen/services/UsuarioService.java
+++ src/main/java/luceroraul/stacktrace/examen/services/UsuarioService.java
@@ -1,12 +1,25 @@
 package luceroraul.stacktrace.examen.services;
 
+import luceroraul.stacktrace.examen.entities.Billetera;
 import luceroraul.stacktrace.examen.entities.Usuario;
+import luceroraul.stacktrace.examen.repositories.BilleteraRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService extends ServiceABM<Usuario>{
+    @Autowired
+    private BilleteraRepository billeteraRepository;
     @Override
     protected Class<Usuario> recuperarClaseGenerica() {
         return Usuario.class;
+    }
+
+    @Override
+    public ResponseEntity<Object> crear(Usuario elemento) {
+        ResponseEntity<Object> respuesta = super.crear(elemento);
+        billeteraRepository.save(new Billetera(elemento,null));
+        return respuesta;
     }
 }

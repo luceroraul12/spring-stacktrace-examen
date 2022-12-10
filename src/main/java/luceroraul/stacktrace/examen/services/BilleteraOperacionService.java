14526e881cc923b804575e074128ec27334997a6
Lucero Raul <luceroraul12@gmail.com>
2022-12-10 11:31:21 -0300

    add metodos

--- src/main/java/luceroraul/stacktrace/examen/services/BilleteraOperacionService.java
+++ src/main/java/luceroraul/stacktrace/examen/services/BilleteraOperacionService.java
@@ -2,30 +2,57 @@ package luceroraul.stacktrace.examen.services;
 
 import luceroraul.stacktrace.examen.entities.Activo;
 import luceroraul.stacktrace.examen.repositories.ActivoRepository;
-import luceroraul.stacktrace.examen.request.MonedaCriptoOperacionPeticion;
+import luceroraul.stacktrace.examen.request.PeticionDeposito;
+import luceroraul.stacktrace.examen.request.PeticionIntercambio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
+@Service
 public class BilleteraOperacionService {
 
     @Autowired
     ActivoRepository activoRepository;
 
-    public Activo depositar(MonedaCriptoOperacionPeticion peticion){
+    public Activo depositar(PeticionDeposito peticion) throws Exception {
         Activo resultado;
-        Activo divisaAlmacenada = activoRepository.findById(peticion.getIdActivoDestino()).get();
-
-        resultado = realizarDeposito(divisaAlmacenada,
-                peticion.getCantidadOperable());
+        Long idActivoOrigen;
+        Double cantidad;
+        idActivoOrigen = peticion.getIdActivoOrigen();
+        cantidad = peticion.getCantidadOperable();
+        resultado = realizarIncremento(
+                activoRepository.findById(idActivoOrigen).orElseThrow(),
+                cantidad);
         return resultado;
     }
 
-    public Activo realizarDeposito(
-            Activo divisaDestino,
-            Double cantidadOperable) {
-            divisaDestino.setCantidadAdquirida(
-                    divisaDestino.getCantidadAdquirida() + cantidadOperable
-            );
-            return divisaDestino;
+    public Map<String, Activo> intercambiar(PeticionIntercambio peticion){
+        Activo resultado;
+        Long idActivoOrigen;
+        Double cantidad;
+        idActivoOrigen = peticion.getIdActivoOrigen();
+        cantidad = peticion.getCantidadOperable();
+        resultado = realizarIncremento(
+                activoRepository.findById(idActivoOrigen).orElseThrow(),
+                cantidad);
+        return resultado;
     }
+
+    public Activo realizarIncremento(Activo activo, Double cantidad) {
+        activo.setCantidadAdquirida(activo.getCantidadAdquirida() + cantidad);
+        return activo;
+    }
+
+    public Activo realizarDecremento(Activo activo, Double cantidad) {
+        activo.setCantidadAdquirida(activo.getCantidadAdquirida() - cantidad);
+        return activo;
+    }
+
+    public boolean tieneMontoSuficiente(Long idActivoOrigen, Double cantidad) {
+        Activo activo = activoRepository.findById(idActivoOrigen).orElseThrow();
+        return activo.getCantidadAdquirida() >= cantidad;
+    }
+
 
 }

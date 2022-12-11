d0492bc22071c384c46764919fbc652b736b79a2
Lucero Raul <luceroraul12@gmail.com>
2022-12-11 10:08:12 -0300

    cambios de entidad -> enum y simplificacion por enunciado

--- src/main/java/luceroraul/stacktrace/examen/init/BaseDeDatosIncial.java
+++ src/main/java/luceroraul/stacktrace/examen/init/BaseDeDatosIncial.java
@@ -3,8 +3,6 @@ package luceroraul.stacktrace.examen.init;
 import jakarta.annotation.PostConstruct;
 import luceroraul.stacktrace.examen.entities.*;
 import luceroraul.stacktrace.examen.repositories.MonedaCriptoRepository;
-import luceroraul.stacktrace.examen.repositories.MonedaTradicionalRepository;
-import luceroraul.stacktrace.examen.repositories.SexoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,23 +10,11 @@ import org.springframework.context.annotation.Configuration;
 public class BaseDeDatosIncial {
 
     @Autowired
-    SexoRepository sexoRepository;
-
-    @Autowired
-    MonedaTradicionalRepository monedaTradicionalRepository;
-
-    @Autowired
     MonedaCriptoRepository monedaCriptoRepository;
 
 
     @PostConstruct
     public void cargarBaseDeDatos(){
-        if(sexoRepository.count() == 0){
-            cargarTablaSexo();
-        }
-        if (monedaTradicionalRepository.count() == 0){
-            cargarTablaMonedaTradicional();
-        }
         if (monedaCriptoRepository.count() == 0){
             cargarTablaMonedaCripto();
         }
@@ -37,19 +23,5 @@ public class BaseDeDatosIncial {
     private void cargarTablaMonedaCripto() {
         monedaCriptoRepository.save(new MonedaCripto("BTC", 100.0));
         monedaCriptoRepository.save(new MonedaCripto("ETH", 20.0));
-    }
-
-    private void cargarTablaMonedaTradicional() {
-        monedaTradicionalRepository.save(new MonedaTradicional("USD", 1.0));
-        monedaTradicionalRepository.save(new MonedaTradicional("ARGS", 169.81));
-    }
-
-
-    private void cargarTablaSexo() {
-
-        sexoRepository.save(new Sexo("masculino"));
-        sexoRepository.save(new Sexo("femenino"));
-        sexoRepository.save(new Sexo("otro"));
-        sexoRepository.save(new Sexo("no incluir"));
     }
 }

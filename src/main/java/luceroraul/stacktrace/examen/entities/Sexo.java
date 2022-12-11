c623d07a2c9aed1933a689bbd1daca25dbbe441f
Lucero Raul <luceroraul12@gmail.com>
2022-12-11 10:07:27 -0300

    sexo entity -> enum

--- src/main/java/luceroraul/stacktrace/examen/entities/Sexo.java
+++ src/main/java/luceroraul/stacktrace/examen/entities/Sexo.java
@@ -1,20 +1,5 @@
 package luceroraul.stacktrace.examen.entities;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import lombok.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Getter
-@Setter
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
-public class Sexo extends Identificable {
-    @Column(name = "nombre", unique = true)
-    private String nombre;
+public enum Sexo  {
+    HOMBRE, MUJER, OTRO
 }

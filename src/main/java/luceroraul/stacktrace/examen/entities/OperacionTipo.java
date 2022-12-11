527013cd6aa11ddf28cf13d1c0e8f426cf35d246
Lucero Raul <luceroraul12@gmail.com>
2022-12-11 08:58:09 -0300

    operacion tipo entity -> enum

--- src/main/java/luceroraul/stacktrace/examen/entities/OperacionTipo.java
+++ src/main/java/luceroraul/stacktrace/examen/entities/OperacionTipo.java
@@ -1,18 +1,9 @@
 package luceroraul.stacktrace.examen.entities;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
-public class OperacionTipo extends Identificable{
-    @Column(name = "tipo", unique = true)
-    private String tipo;
+
+
+public enum OperacionTipo{
+    DEPOSITO, INTERCAMBIO
 }
--- src/main/java/luceroraul/stacktrace/examen/repositories/OperacionTipoRepository.java
+++
@@ -1,7 +0,0 @@
-package luceroraul.stacktrace.examen.repositories;
-
-import luceroraul.stacktrace.examen.entities.OperacionTipo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OperacionTipoRepository extends JpaRepository<OperacionTipo, Long> {
-}

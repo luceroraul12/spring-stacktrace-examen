4e5bb5488529002f62e1971238a6cc71079ae8f6
Lucero Raul <luceroraul12@gmail.com>
2022-12-11 10:07:14 -0300

    deletes

--- src/main/java/luceroraul/stacktrace/examen/repositories/MonedaTradicionalRepository.java
+++
@@ -1,7 +0,0 @@
-package luceroraul.stacktrace.examen.repositories;
-
-import luceroraul.stacktrace.examen.entities.MonedaTradicional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MonedaTradicionalRepository extends JpaRepository<MonedaTradicional, Long> {
-}
--- src/main/java/luceroraul/stacktrace/examen/repositories/SexoRepository.java
+++
@@ -1,7 +0,0 @@
-package luceroraul.stacktrace.examen.repositories;
-
-import luceroraul.stacktrace.examen.entities.Sexo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SexoRepository extends JpaRepository<Sexo, Long> {
-}

a7fb54c73dfed3eb94669e75b22660b96e504492
Lucero Raul <luceroraul12@gmail.com>
2022-12-12 14:30:26 -0300

    adaptando tipo de retorno

--- src/main/java/luceroraul/stacktrace/examen/controllers/divisas/OperacionesDivisasEnBilletera.java
+++ src/main/java/luceroraul/stacktrace/examen/controllers/divisas/OperacionesDivisasEnBilletera.java
@@ -1,15 +1,14 @@
 package luceroraul.stacktrace.examen.controllers.divisas;
 
-import luceroraul.stacktrace.examen.entities.BilleteraDto.ActivoDto;
 import luceroraul.stacktrace.examen.request.PeticionDeposito;
 import luceroraul.stacktrace.examen.request.PeticionIntercambio;
-import luceroraul.stacktrace.examen.responses.Respuesta;
+import luceroraul.stacktrace.examen.responses.Respuesta.Body;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Map;
 
 public interface OperacionesDivisasEnBilletera {
 
-    ResponseEntity<Respuesta<ActivoDto>> depositarCantidad(PeticionDeposito peticion) throws Exception;
-    ResponseEntity<Respuesta<Map<String, ActivoDto>>> intercambiarCantidad(PeticionIntercambio Peticion) throws Exception;
+    ResponseEntity<Body> depositarCantidad(PeticionDeposito peticion) throws Exception;
+    ResponseEntity<Body> intercambiarCantidad(PeticionIntercambio Peticion) throws Exception;
 }

45fc49726390fad34cc294d355fb396d8c8949e1
Lucero Raul <luceroraul12@gmail.com>
2022-12-12 12:57:27 -0300

    fix id generador auto -> identity

--- src/main/java/luceroraul/stacktrace/examen/entities/Identificable.java
+++ src/main/java/luceroraul/stacktrace/examen/entities/Identificable.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @MappedSuperclass
 public abstract class Identificable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 }

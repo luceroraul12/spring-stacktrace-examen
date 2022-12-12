16335c95f7e5b4dc5ddb92d9aac08c69f24777f6
Lucero Raul <luceroraul12@gmail.com>
2022-12-12 08:08:50 -0300

    fix arg

--- src/main/java/luceroraul/stacktrace/examen/controllers/divisas/OperacionesDivisasEnBilletera.java
+++ src/main/java/luceroraul/stacktrace/examen/controllers/divisas/OperacionesDivisasEnBilletera.java
@@ -1,10 +1,11 @@
 package luceroraul.stacktrace.examen.controllers.divisas;
 
 import luceroraul.stacktrace.examen.request.PeticionDeposito;
+import luceroraul.stacktrace.examen.request.PeticionIntercambio;
 import org.springframework.http.ResponseEntity;
 
 public interface OperacionesDivisasEnBilletera {
 
     ResponseEntity<Object> depositarCantidad(PeticionDeposito peticion);
-    ResponseEntity<Object> intercambiarCantidad(PeticionDeposito Peticion);
+    ResponseEntity<Object> intercambiarCantidad(PeticionIntercambio Peticion);
 }

776a77bd1c29382e97b8229936884d1a623728a3
Lucero Raul <luceroraul12@gmail.com>
2022-12-12 12:58:24 -0300

    add consulta por billetera y usuario

--- src/main/java/luceroraul/stacktrace/examen/services/BilleteraService.java
+++ src/main/java/luceroraul/stacktrace/examen/services/BilleteraService.java
@@ -5,12 +5,14 @@ import luceroraul.stacktrace.examen.entities.BilleteraDto;
 import luceroraul.stacktrace.examen.entities.MonedaCripto;
 import luceroraul.stacktrace.examen.entities.Activo;
 import luceroraul.stacktrace.examen.repositories.BilleteraRepository;
+import luceroraul.stacktrace.examen.responses.Respuesta;
 import luceroraul.stacktrace.examen.util.BilleteraUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -48,5 +50,29 @@ public class BilleteraService extends ServiceABM<Billetera>{
         return new Activo(moneda, 0.0, billeteraSeleccionada);
     }
 
+
+    public ResponseEntity<Respuesta<Double>> consultarSaldoPorBilletera(Long id) {
+        Billetera billetera = billeteraRepository.findById(id).orElseThrow();
+        Respuesta<Double> resultado = new Respuesta<>(
+                billeteraUtil.obtenerSaldoBilleteraEnPesos(Collections.singletonList(billetera)),
+                "Consulta de saldo por billetera exitosa"
+        );
+
+        return new ResponseEntity<>(resultado, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Respuesta<Double>> consultarSaldoPorUsuario(Long id){
+        List<Billetera> billeteras = billeteraRepository.obtenerTodasLasBilleterasDeUsuario(id);
+        Respuesta<Double> resultado;
+        if (billeteras.size() != 0){
+            resultado = new Respuesta<>(
+                    billeteraUtil.obtenerSaldoBilleteraEnPesos(billeteras),
+                    "Consulta de saldo por usuario exitosa"
+            );
+        } else {
+            resultado = new Respuesta<>(null, "no existe billetera con id: "+id);
+        }
+        return new ResponseEntity<>(resultado, HttpStatus.OK);
+    }
 
 }

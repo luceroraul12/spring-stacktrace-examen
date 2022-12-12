8fa0b7ef224e10c569f8b83b55ebd62f42916f19
Lucero Raul <luceroraul12@gmail.com>
2022-12-12 14:30:06 -0300

    mejorando attrs de respuesta

--- src/main/java/luceroraul/stacktrace/examen/responses/Respuesta.java
+++ src/main/java/luceroraul/stacktrace/examen/responses/Respuesta.java
@@ -3,11 +3,30 @@ package luceroraul.stacktrace.examen.responses;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Var;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class Respuesta<Entidad>{
-    private Entidad body;
-    private String mensaje;
+public class Respuesta{
+    private Body body;
+    private HttpStatus estadoHttp;
+
+    public Respuesta(Object body, String mensaje, HttpStatus estadoHttp) {
+        this.body = new Body(body, mensaje);
+        this.estadoHttp = estadoHttp;
+    }
+    public ResponseEntity<Body> getResponseEntity(){
+        return new ResponseEntity<>(body, estadoHttp);
+    }
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public class Body {
+        private Object body;
+        private String mensaje;
+
+    }
 }

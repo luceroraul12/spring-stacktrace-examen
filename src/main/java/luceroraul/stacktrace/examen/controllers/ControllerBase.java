5455d5a1100dbdc8b67f4f84a77e9e7706d126b8
Lucero Raul <luceroraul12@gmail.com>
2022-12-13 15:46:03 -0300

    cambio de body -> param al eliminar

--- src/main/java/luceroraul/stacktrace/examen/controllers/ControllerBase.java
+++ src/main/java/luceroraul/stacktrace/examen/controllers/ControllerBase.java
@@ -8,10 +8,7 @@ import luceroraul.stacktrace.examen.services.ServiceABM;
 import luceroraul.stacktrace.examen.util.BaseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -31,8 +28,8 @@ public abstract class ControllerBase<Entidad extends Identificable, ClaseDTO ext
     }
 
     @DeleteMapping("baja")
-    public ResponseEntity<Body> eliminar(@RequestBody ClaseDTO elemento){
-        return  serviceABM.eliminar(elemento);
+    public ResponseEntity<Body> eliminar(@RequestParam(value = "id-elemento") Long id){
+        return  serviceABM.eliminar(id);
     }
 
 }

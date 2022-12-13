e34b1da9290f112b45867f22ccfa630a96b451a2
Lucero Raul <luceroraul12@gmail.com>
2022-12-13 16:50:46 -0300

    add metodo para saber si una moneda ya estaba asignada a dicha billetera

--- src/main/java/luceroraul/stacktrace/examen/services/ActivoService.java
+++ src/main/java/luceroraul/stacktrace/examen/services/ActivoService.java
@@ -2,10 +2,19 @@ package luceroraul.stacktrace.examen.services;
 
 import luceroraul.stacktrace.examen.entities.Activo;
 import luceroraul.stacktrace.examen.entities.ActivoDTO;
+import luceroraul.stacktrace.examen.repositories.ActivoRepository;
+import luceroraul.stacktrace.examen.repositories.BilleteraRepository;
+import luceroraul.stacktrace.examen.repositories.MonedaCriptoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ActivoService extends ServiceABM<Activo, ActivoDTO>{
+    @Autowired
+    BilleteraRepository billeteraRepository;
+
+    @Autowired
+    ActivoRepository activoRepository;
     @Override
     protected Class<Activo> recuperarClaseGenerica() {
         return Activo.class;
@@ -13,8 +22,20 @@ public class ActivoService extends ServiceABM<Activo, ActivoDTO>{
 
     @Override
     protected boolean cumpleCondicionDeCreacion(ActivoDTO elemento) {
-        Boolean existeEnSistema = repository.existsById(elemento.getId());
-        return !existeEnSistema;
+        boolean esBilleteraValida;
+        boolean esMonedaYaAsignada;
+        boolean resultado;
+        try {
+            esBilleteraValida = billeteraRepository.existsById(elemento.getIdBilletera());
+        } catch (Exception e) {
+            esBilleteraValida = false;
+        }
+            esMonedaYaAsignada = activoRepository.existeMonedaParaDichabilletera(
+                    elemento.getIdMonedaCripto(),
+                    elemento.getIdBilletera()
+            );
+        resultado = !esMonedaYaAsignada & esBilleteraValida;
+        return resultado;
     }
 
 

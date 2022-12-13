89ac7215f9e2202ebbe4ab9ace36830c029d0ac4
Lucero Raul <luceroraul12@gmail.com>
2022-12-13 16:50:36 -0300

    add metodo para saber si una moneda ya estaba asignada a dicha billetera

--- src/main/java/luceroraul/stacktrace/examen/repositories/ActivoRepository.java
+++ src/main/java/luceroraul/stacktrace/examen/repositories/ActivoRepository.java
@@ -2,6 +2,12 @@ package luceroraul.stacktrace.examen.repositories;
 
 import luceroraul.stacktrace.examen.entities.Activo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ActivoRepository extends JpaRepository<Activo, Long> {
+    @Query("select (count(a) > 0) from Activo a where a.monedaCripto.id = :id and a.billetera.id = :id1")
+    boolean existeMonedaParaDichabilletera(@Param("id") Long idMonedaCripto, @Param("id1") Long idBilletera);
+
+
 }

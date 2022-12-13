62a59d160a5a06e8dcf84c8db1a3bd3064b5f63d
Lucero Raul <luceroraul12@gmail.com>
2022-12-13 11:46:28 -0300

    fix monedacriptoDTO

--- src/main/java/luceroraul/stacktrace/examen/entities/MonedaCriptoDTO.java
+++ src/main/java/luceroraul/stacktrace/examen/entities/MonedaCriptoDTO.java
@@ -1,7 +1,12 @@
 package luceroraul.stacktrace.examen.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 
+@Getter
+@Setter
 public class MonedaCriptoDTO extends BaseDTO{
     private String nombre;
     private Double relacionPeso;

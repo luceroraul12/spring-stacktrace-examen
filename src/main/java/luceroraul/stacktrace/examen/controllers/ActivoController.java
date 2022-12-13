f179b4086bb68d7c82aa520eddf0e3d8ffae12d2
Lucero Raul <luceroraul12@gmail.com>
2022-12-13 16:51:03 -0300

    adaptando entrada para crear

--- src/main/java/luceroraul/stacktrace/examen/controllers/ActivoController.java
+++ src/main/java/luceroraul/stacktrace/examen/controllers/ActivoController.java
@@ -2,10 +2,20 @@ package luceroraul.stacktrace.examen.controllers;
 
 import luceroraul.stacktrace.examen.entities.Activo;
 import luceroraul.stacktrace.examen.entities.ActivoDTO;
+import luceroraul.stacktrace.examen.responses.Respuesta;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("billeteras/activos")
 public class ActivoController extends ControllerBase<Activo, ActivoDTO> {
+
+    @Override
+    public ResponseEntity<Respuesta.Body> crear(@RequestBody ActivoDTO elemento) {
+        elemento.setCantidadAdquirida(0.0);
+        elemento.setId(null);
+        return super.crear(elemento);
+    }
 }

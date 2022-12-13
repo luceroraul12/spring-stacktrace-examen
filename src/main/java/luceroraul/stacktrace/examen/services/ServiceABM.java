d191dd728762273c63b63bb64cc42cde08d1f761
Lucero Raul <luceroraul12@gmail.com>
2022-12-13 20:24:19 -0300

    add documentacion de ServiceABM

--- src/main/java/luceroraul/stacktrace/examen/services/ServiceABM.java
+++ src/main/java/luceroraul/stacktrace/examen/services/ServiceABM.java
@@ -10,15 +10,33 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+/**
+ * Servicio abstracto base para realizar alta, baja, modificacion de entidades
+ * @param <Entidad> Clase utilizada con JPA
+ * @param <ClaseDTO> Clase que se retorna al usuario del servicio
+ */
 public abstract class ServiceABM<Entidad extends Identificable, ClaseDTO extends BaseDTO> {
+    /**
+     * Implementacion de inyeccion de dependencia por medio del uso de genericos.
+     * En este caso parten de JpaRepository para poder realizar ABM.
+     */
     @Autowired
     protected JpaRepository<Entidad, Long> repository;
 
+    /**
+     * Clase utilitaria generica para cada implementacion. En ella se encontraran los metodos necesarios para hacer conversiones respecto a dicha entidad.
+     */
     @Autowired
     protected BaseUtil<Entidad, ClaseDTO> baseUtil;
     protected ServiceABM() {
     }
 
+    /**
+     * Metodo encargado de hacer persistir el argumento de entrada.
+     * tener en cuenta {@link ServiceABM#cumpleCondicionDeCreacion(BaseDTO)}
+     * @param elemento es el objecto con los datos necesarios para hacer persistirlos.
+     * @return ResponseEntity del tipo {@link Body}
+     */
     public ResponseEntity<Body> crear(ClaseDTO elemento){
         Respuesta respuesta;
         if(!cumpleCondicionDeCreacion(elemento)){
@@ -31,7 +49,11 @@ public abstract class ServiceABM<Entidad extends Identificable, ClaseDTO extends
         return respuesta.getResponseEntity();
     }
 
-
+    /**
+     * Metodo encargado de eliminar un registro de la base de dato en funcion a su identificador
+     * @param id identificador
+     * @return ResponseEntity del tipo {@link Body}
+     */
     public ResponseEntity<Body> eliminar(Long id){
         Respuesta respuesta;
         try {
@@ -49,7 +71,14 @@ public abstract class ServiceABM<Entidad extends Identificable, ClaseDTO extends
         return respuesta.getResponseEntity();
     }
 
-    public ResponseEntity<Body> modificar(ClaseDTO elementoParcial) throws Exception {
+    /**
+     * Metodo encargado de realizar la modificacion de un registro de la base de datos.
+     * El argumento puede ser con datos completos o parcial. Esto se debe a que primero va a buscar el registro y luego lo fusionara con la informacion del argumento.
+     * Tener en cuenta {@link BaseUtil#fusionarDTOyEntidad(Object, Object)}
+     * @param elementoParcial dato completo o parcial a guardar
+     * @return ResponseEntity del tipo {@link Body}
+     */
+    public ResponseEntity<Body> modificar(ClaseDTO elementoParcial){
         Respuesta respuesta;
         Long id = elementoParcial.getId();
 
@@ -73,6 +102,11 @@ public abstract class ServiceABM<Entidad extends Identificable, ClaseDTO extends
 
     protected abstract Class<Entidad> recuperarClaseGenerica();
 
+    /**
+     * Metodo abstracto a implementar por las implementaciones donde se debe verificar si la informacion de entrada es valida para ser persistida en la base de datos.
+     * @param elemento objeto de entrada
+     * @return si cumple o no para ser creado
+     */
     protected abstract boolean cumpleCondicionDeCreacion(ClaseDTO elemento);
 
 

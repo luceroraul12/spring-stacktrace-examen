623863eec56760007055e3aeb67103a3d92d14fa
Lucero Raul <luceroraul12@gmail.com>
2022-12-13 15:56:24 -0300

    add id bileltera

--- src/main/java/luceroraul/stacktrace/examen/entities/ActivoDTO.java
+++ src/main/java/luceroraul/stacktrace/examen/entities/ActivoDTO.java
@@ -8,12 +8,14 @@ import java.io.Serializable;
 @Getter
 @Setter
 public class ActivoDTO extends BaseDTO implements Serializable {
+    private Long idBilletera;
     private Long idMonedaCripto;
     private String monedaCriptoNombre;
     private Double cantidadAdquirida;
 
-    public ActivoDTO(Long id, Long idMonedaCripto, String monedaCriptoNombre, Double cantidadAdquirida) {
+    public ActivoDTO(Long id, Long idBilletera, Long idMonedaCripto, String monedaCriptoNombre, Double cantidadAdquirida) {
         super(id);
+        this.idBilletera = idBilletera;
         this.idMonedaCripto = idMonedaCripto;
         this.monedaCriptoNombre = monedaCriptoNombre;
         this.cantidadAdquirida = cantidadAdquirida;

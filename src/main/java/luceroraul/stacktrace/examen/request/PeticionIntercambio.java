6aa526a462e7a348d24600bde7c0118cb5ced704
Lucero Raul <luceroraul12@gmail.com>
2022-12-13 22:47:16 -0300

    Add documentacion a PeticionIntercambio

--- src/main/java/luceroraul/stacktrace/examen/request/PeticionIntercambio.java
+++ src/main/java/luceroraul/stacktrace/examen/request/PeticionIntercambio.java
@@ -4,9 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 import luceroraul.stacktrace.examen.entities.Operacion.OperacionTipo;
 
+/**
+ * Peticion necesaria para realizar un intercambio de monedas. Extiende de {@link PeticionDeposito} debido a que necesita los mismos atributos.
+ */
 @Getter
 @Setter
 public class PeticionIntercambio extends PeticionDeposito{
+    /**
+     * En este caso, se le agrega el identificador del {@link luceroraul.stacktrace.examen.entities.Activo} de donde debe descontarse dicha cantidad.
+     */
     private Long idActivoOrigen;
 
     public PeticionIntercambio(OperacionTipo operacionTipo, Double cantidadOperable, Long idActivoDestino, Long idActivoOrigen) {

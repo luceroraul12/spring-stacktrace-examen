0698fb7fee9c02bee4d2cd5b1799d50bb42cce15
Lucero Raul <luceroraul12@gmail.com>
2022-12-13 22:41:31 -0300

    Add documentacion a Respuesta

--- src/main/java/luceroraul/stacktrace/examen/responses/Respuesta.java
+++ src/main/java/luceroraul/stacktrace/examen/responses/Respuesta.java
@@ -7,11 +7,20 @@ import org.aspectj.weaver.ast.Var;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+/**
+ * Es una respuesta modelo para todos los tipos de respuesta
+ */
 @Getter
 @Setter
 @AllArgsConstructor
 public class Respuesta{
+    /**
+     * Es la respuesta objeto de la API
+     */
     private Body body;
+    /**
+     * Codigo de mensaje HTTP
+     */
     private HttpStatus estadoHttp;
 
     public Respuesta(Object body, String mensaje, HttpStatus estadoHttp) {
@@ -21,11 +30,23 @@ public class Respuesta{
     public ResponseEntity<Body> getResponseEntity(){
         return new ResponseEntity<>(body, estadoHttp);
     }
+
+    /**
+     * Respuesta util de la API
+     */
     @Getter
     @Setter
     @AllArgsConstructor
     public class Body {
+        /**
+         * Al poder llegar a devolver diferentes tipos de informacion, esta tipado como Object pero normalmente puede ser:
+         * - clases DTO
+         * - null
+         */
         private Object body;
+        /**
+         * Mensaje que indica que paso al recuperar la informacion, puede ser afirmativo o negativo
+         */
         private String mensaje;
 
     }

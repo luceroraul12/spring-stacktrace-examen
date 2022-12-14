6c6e62e1225bcfd9d02385cc03ecaa82ff51f878
Lucero Raul <luceroraul12@gmail.com>
2022-12-14 00:26:43 -0300

    Add documentacion swagger a ControllerBase

--- src/main/java/luceroraul/stacktrace/examen/controllers/ControllerBase.java
+++ src/main/java/luceroraul/stacktrace/examen/controllers/ControllerBase.java
@@ -1,5 +1,10 @@
 package luceroraul.stacktrace.examen.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import luceroraul.stacktrace.examen.entities.BaseDTO;
 import luceroraul.stacktrace.examen.entities.Identificable;
 import luceroraul.stacktrace.examen.responses.Respuesta;
@@ -18,7 +23,6 @@ import java.util.Map;
  * @param <ClaseDTO> Clase dto que se le entrega al cliente
  */
 public abstract class ControllerBase<Entidad extends Identificable, ClaseDTO extends BaseDTO> {
-
     /**
      * Hace referencia a la inyeccion de depedencia por medio de genericos.
      * Ver {@link ServiceABM}.
@@ -31,8 +35,16 @@ public abstract class ControllerBase<Entidad extends Identificable, ClaseDTO ext
      * @param elemento dto con informacion necesaria para crear
      * @return Body
      */
+    @Operation(summary = "Da de alta un elemento de dicho controller")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "creacion realizada con exito",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Body.class)) }),
+            @ApiResponse(responseCode = "202", description = "fue recibido por el sistema pero hay parametros erroneos y por lo tanto se cancela la peticion",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Body.class)))})
     @PostMapping("alta")
     public ResponseEntity<Body> crear(@RequestBody ClaseDTO elemento){
+        elemento.setId(null);
         return serviceABM.crear(elemento);
     }
 
@@ -41,6 +53,13 @@ public abstract class ControllerBase<Entidad extends Identificable, ClaseDTO ext
      * @param elemento dto completo o parcial para realizar una modificacion
      * @return Body
      */
+    @Operation(summary = "permite modificar un elemento de dicho controller")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "modificacion realizada con exito",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Body.class)) }),
+            @ApiResponse(responseCode = "202", description = "fue recibido por el sistema pero hay parametros erroneos y por lo tanto se cancela la peticion",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Body.class)))})
     @PatchMapping("modificar")
     public ResponseEntity<Body> modificar(@RequestBody ClaseDTO elemento){
         return serviceABM.modificar(elemento);
@@ -51,6 +70,13 @@ public abstract class ControllerBase<Entidad extends Identificable, ClaseDTO ext
      * @param id es el identificador enviado por url
      * @return reafirma la eliminacion retornando el identificador
      */
+    @Operation(summary = "Permite eliminar un elemento de dicho controller")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "eliminacion realizada con exito",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Body.class)) }),
+            @ApiResponse(responseCode = "202", description = "fue recibido por el sistema pero hay parametros erroneos y por lo tanto se cancela la peticion",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Body.class)))})
     @DeleteMapping("baja")
     public ResponseEntity<Body> eliminar(@RequestParam(value = "id-elemento") Long id){
         return  serviceABM.eliminar(id);

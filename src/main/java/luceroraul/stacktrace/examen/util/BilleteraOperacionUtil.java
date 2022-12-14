423cae5c3b57a7c16a53bef1b8583567ca17d8b6
Lucero Raul <luceroraul12@gmail.com>
2022-12-13 21:00:19 -0300

    add documentacion de BilleteraOperacionUtil

--- src/main/java/luceroraul/stacktrace/examen/util/BilleteraOperacionUtil.java
+++ src/main/java/luceroraul/stacktrace/examen/util/BilleteraOperacionUtil.java
@@ -3,13 +3,30 @@ package luceroraul.stacktrace.examen.util;
 import luceroraul.stacktrace.examen.entities.Activo;
 import org.springframework.stereotype.Component;
 
+/**
+ * Util enfocado actividades necesarioas para la Operacion de {@link Activo} relacionado a pesos
+ */
 @Component
 public class BilleteraOperacionUtil {
+
+    /**
+     * Incrementa la cantidad del {@link Activo} cuando {@link luceroraul.stacktrace.examen.entities.MonedaCripto} es la misma
+     * @param activo
+     * @param cantidad
+     * @return
+     */
     public Activo realizarIncrementoMismaUnidad(Activo activo, Double cantidad) {
         activo.setCantidadAdquirida(activo.getCantidadAdquirida() + cantidad);
         return activo;
     }
 
+    /**
+     * Incrementa la cantidad del {@link Activo} cuando el activo de origen se encuentra con {@link luceroraul.stacktrace.examen.entities.MonedaCripto} diferente
+     * @param activoOrigen
+     * @param activoDestino
+     * @param cantidadParaIncrementarOriginal
+     * @return
+     */
     public Activo realizarIncrementoDiferentesUnidades(Activo activoOrigen, Activo activoDestino, Double cantidadParaIncrementarOriginal){
         Double precioUnitarioMonedaOrigenEnPesos = activoOrigen.getMonedaCripto().getRelacionDolar();
         Double precioUnitarioMonedaDestinoEnPesos = activoDestino.getMonedaCripto().getRelacionDolar();
@@ -21,15 +38,33 @@ public class BilleteraOperacionUtil {
         return activoDestino;
     }
 
+    /**
+     * Resta cierta cantidad al Activo
+     * @param activo
+     * @param cantidad
+     * @return
+     */
     public Activo realizarReduccion(Activo activo, Double cantidad) {
         activo.setCantidadAdquirida(activo.getCantidadAdquirida() - cantidad);
         return activo;
     }
 
+    /**
+     * Verifica de que el monto sea mayor que la cantidad a restar
+     * @param activo
+     * @param cantidad
+     * @return
+     */
     public boolean tieneMontoSuficiente(Activo activo, Double cantidad) {
         return activo.getCantidadAdquirida() >= cantidad;
     }
 
+    /**
+     * Comprueba que ambos activos tiene la misma {@link luceroraul.stacktrace.examen.entities.MonedaCripto} asignada
+     * @param activoOrigen
+     * @param activoDestino
+     * @return
+     */
     public boolean sonSobreMismaMonedaCripto(Activo activoOrigen, Activo activoDestino) {
         return activoOrigen.getMonedaCripto().equals(activoDestino.getMonedaCripto());
     }

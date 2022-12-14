660f4315acc3ff81ba0e0d683133ec686874449e
Lucero Raul <luceroraul12@gmail.com>
2022-12-13 22:45:28 -0300

    Add documentacion a PeticionDeposito

--- src/main/java/luceroraul/stacktrace/examen/request/PeticionDeposito.java
+++ src/main/java/luceroraul/stacktrace/examen/request/PeticionDeposito.java
@@ -5,11 +5,24 @@ import lombok.Getter;
 import lombok.Setter;
 import luceroraul.stacktrace.examen.entities.Operacion.OperacionTipo;
 
+/**
+ * Clase con informacion necesaria para realizar un deposito.
+ */
 @Getter
 @Setter
 @AllArgsConstructor
 public class PeticionDeposito {
+    /**
+     * Es el tipo de operacion. Ver {@link OperacionTipo}.
+     * Como la API fue diseniada para distingir DEPOSITO de INTERCAMBIO, no es un dato obligatorio cuando se realice una peticion. Solo es util para el sistema al a hora de persisitr, debido a que el mismo lo vuelve a asignar previamente de manera automatica.
+     */
     private OperacionTipo operacionTipo;
+    /**
+     * Es la cantidad que se desea introducir
+     */
     private Double cantidadOperable;
+    /**
+     * Es el identificador del {@link luceroraul.stacktrace.examen.entities.Activo} al que se le quiene depositar
+     */
     private Long idActivoDestino;
 }
